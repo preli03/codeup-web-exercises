a42330bc2ef12177c9f10656cb169c0f3dadd1ae
Patrick Mireles <patrickm.mireles@gmail.com>
2023-03-13 09:24:19 -0500

    new enrolle students

--- js/external.js
+++ js/external.js
@@ -31,14 +31,7 @@ var pricePerDay = 3;
 var totalPrice = ( littleMermaid + brotherBear + hercules) * pricePerDay
 console.log("You will have to pay " + totalPrice + " to rent the movies")
 
-
-function priceForMovies(x, y, z){
-  return littleMermaid + brotherBear + hercules;
-}
-
-console.log(priceForMovies(littleMermaid, brotherBear, hercules));
-
-alert( "totalPrice )
+alert('The total cost is $' + totalPrice +' for your movies')
 
 // var days = ["littleMermaid" + "brotherBear" + "hercules"];
 
@@ -51,24 +44,34 @@ var aHours = 4;
 var fbHours = 10;
 
 var gHours = Number(prompt('How many hrs did you work for google?'));
-consol.log(gHours)
+console.log(gHours);
 
 var aHours = Number(prompt('How many hrs did you work for aHours?'));
-consol.log(aHours)
+console.log(aHours);
 
 var fbHours = Number(prompt('How many hrs did you work for fbHours?'));
-consol.log(fbHours)
+console.log(fbHours);
 
 
-var paycheck = (google * gHours) + ( amazon * aHours ) + ( facebook * fbHours )
-console.log("Your paycheck will be " + paycheck + "!")
+var paycheck = (google * gHours) + ( amazon * aHours ) + ( facebook * fbHours );
+console.log("Your paycheck will be " + paycheck + "!");
+
+alert('Your weekly income will be $' + paycheck );
+
 
 //A student can be enrolled to a class only if the class is not full and
 // the class schedule does not conflict with her current schedule.
-if ((cClass.students.length < cClass.maxEnrolled) && (student.classSchedule.indexOf(cClass.startTime) == -1))
-{
-  student.enroll();
-}
+var students = prompt('How many students are in this class? 25 students per classroom.')
+var roomAvailable = students < 25;
+console.log(roomAvailable)
+var classDay = prompt('What day is the class?')
+var availableDays = ['monday', 'tuesday', 'thursday', 'friday'];
+var checkSchedule = availableDays.includes(classDay.toLowerCase())
+console.log(checkSchedule)
+var enrolled = checkSchedule && roomAvailable
+console.log(enrolled)
+alert('📚Enrollment status: ' +enrolled)
+
 
 // A product offer can be applied only if a person buys more than 2 items,
 // and the offer has not expired. Premium members do not need to buy a specific amount of products.
