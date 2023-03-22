40a4a189f8f33b232d39b01da16af6751ab73e67
Patrick Mireles <patrickm.mireles@gmail.com>
2023-03-22 14:17:49 -0500

    Hello World

--- js/objects.js
+++ js/objects.js
@@ -1,5 +1,62 @@
-(function() {
-    "use strict";
+"use strict";
+var cars = [
+    {
+        make: "Toyota",
+        model: "Camry",
+        features: ["Automatic Windows", "Bluetooth Connectivity", "GPS Navigation"],
+        owner: {
+            name: "Jane Doe",
+            age: 30
+        }
+    },
+    {
+        make: "Honda",
+        model: "Accord",
+        features: ["Automatic Windows", "Great Gas Mileage", "AM/FM Radio"],
+        owner: {
+            name: "John Doe",
+            age: 31
+        }
+    }
+];
+
+console.log("The first car is a " + cars[0].make + " " + cars[0].model + ".");
+console.log("The owner of the second car is " + cars[1].owner.name + ".");
+
+console.log("Here are all the features of all the cars:");
+cars.forEach(function(car) {
+    car.features.forEach(function(feature) {
+        console.log(feature);
+    });
+});
+
+(function () {
+
+    var car = {};
+    car.make = "Toyota";
+    car.model = "Camry";
+
+// create a honk method on the car object
+    car.honk = function () {
+        alert("Honk! Honk!");
+    };
+
+// honk the horn
+    car.honk();
+
+
+    var car = {};
+    car.make = "Toyota";
+    car.model = "Camry";
+
+// create a logMakeModel method on the car object
+    car.logMakeModel = function () {
+        console.log("Car make/model is: " + this.make + " " + this.model);
+    };
+
+// log the make/model
+    car.logMakeModel();
+
 
     /**
      * TODO:
@@ -12,6 +69,10 @@
      *  > console.log(person.lastName) // "Sanchez"
      */
 
+    var person = {
+        firstName: 'Patrick',
+        lastName: 'Mahomes',
+    }
     /**
      * TODO:
      * Add a sayHello method to the person object that returns a greeting using
@@ -21,7 +82,10 @@
      * Example
      * > console.log(person.sayHello()) // "Hello from Rick Sanchez!"
      */
-
+    //var sayHello = function () {
+    //return "Hello Patrick Mahomes!"
+    //}
+    //console.log(person.sayHello)
     /** TODO:
      * HEB has an offer for the shoppers that buy products amounting to
      * more than $200. If a shopper spends more than $200, they get a 12%
@@ -36,12 +100,41 @@
      * and console.log the relevant messages for each person
      */
 
-    // var shoppers = [
-    //     {name: 'Cameron', amount: 180},
-    //     {name: 'Ryan', amount: 250},
-    //     {name: 'George', amount: 320}
-    // ];
+        //for (var C = 180; R = 250; G = 320){
+        //     var car = ''
+        //     for (var g = 0; g < i; g++){
+        //         car += i;
+        //     }
+        //     console.log('all items will be placed inside the car '  +car)
+        // }
+    var shoppers = [
+            {name: 'Cameron', amount: 180},
+            {name: 'Ryan', amount: 250},
+            {name: 'George', amount: 320}
+        ];
 
+    //shoppers.forEach(function (shoppers){
+    //console.log(shoppers.name)
+    // })
+
+    shoppers.forEach(function(shoppers) {
+        if (shopper.amount <= 200) {
+            var discountAmount = shopper.amount * 12;
+            var newTotal = shopper.amount - discountAmount;
+            console.log("Name: " + shopper.name + ", Amount: " + shopper.amount + ',Discount:' + discountAmount + ", New Total: " + newTotal);
+        } else{
+            console.log('Sorry you do not recieve a discount')
+        }
+    });
+
+
+
+    //shoppers.forEach((name,) => {
+    //console.log(shoppers.name);
+    //console.log(shoppers.amount)
+
+
+    // })
     /** TODO:
      * Create an array of objects that represent books and store it in a
      * variable named `books`. Each object should have a title and an author
@@ -53,8 +146,48 @@
      * > console.log(books[0].title) // "The Salmon of Doubt"
      * > console.log(books[0].author.firstName) // "Douglas"
      * > console.log(books[0].author.lastName) // "Adams"
-     */
-
+     */ //starship titanic terry jones, gareth roberts-doctor who:Shada, disenchanted - kelley armstrong
+        //realms of Tolkien -JRR tolkien, star wars - alan dean foster
+    var books = [
+            {
+                title: 'The Salmon of Doubt',
+                author: {
+                    firstName: 'Douglas',
+                    lastName: 'Adams'
+                },
+            }, {
+                title: 'Doctor Who:Shada',
+                author: {
+                    firstName: 'Gareth',
+                    lastName: 'Roberts'
+                },
+            }, {
+                title: "Disenchanted",
+                author: {
+                    firstName: 'Kelley',
+                    lastName: 'Armstrong',
+                }
+            }, {
+                title: 'Realms of Tolkien',
+                author: {
+                    firstName: 'JRR',
+                    lastName: 'Tolkien'
+                },
+            }, {
+                title: 'Star Wars',
+                author: {
+                    firstName: 'Alan Dean',
+                    lastName: 'Foster'
+                },
+            }];
+    //books.forEach(book[2])
+    console.log(books[0].author.lastName + ' ' + books[0].title)
+    console.log(books[1].author.lastName + ' ' + books[1].title)
+    console.log(books[2].author.lastName + ' ' + books[2].title)
+    console.log(books[3].author.lastName + ' ' + books[3].title)
+    console.log(books[4].author.lastName + ' ' + books[4].title)
+    //console.log(books[0].author.firstName)
+    // console.log(books[0].author.lastName)
     /**
      * TODO:
      * Loop through the books array and output the following information about
@@ -77,9 +210,17 @@
      *      Title: A Brief History of Time
      *      Author: Stephen Hawking
      *      ---
-     *      ...
+     *
      */
+        //for(var i = 0; i < books.length; i ++){
+           // console.log()
+    //showBookInfo(books[i]);}
+   // }
+    //books.title.forEach(function(m)
 
+    // {console.log(books)
+    //});
+    //console.log(book('the index' + 'the book title' + 'fullName');
     /**
      * Bonus:
      * - Create a function named `createBook` that accepts a title and author
@@ -90,5 +231,22 @@
      *   outputs the information described above. Refactor your loop to use your
      *   `showBookInfo` function.
      */
+                function createBook(title,author){
+                return {
+                    title: 'Star Wars',
+                    author: {firstName: 'Alan Dean',
+                        lastName: 'Foster'}
+                }
 
+    }
+    //forEach(function(element, index, array)
+    //createBook = function (title first name lastname){
+    //  return as object
+    //}
+        //console.log(createBook)": " + this.index + " " + this.author);
+    //showBookInfo = function ( title first name lastname){
+    //}
+   // };
+    //console.log(showBookInfo)
+    //function createBook(title + '' + author.lastNAme)
 })();
