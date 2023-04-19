5e6b07d5e788c0f74d4662797b45926bf31ea0d9
Patrick Mireles <patrickm.mireles@gmail.com>
2023-04-19 11:04:57 -0500

    added jquery

--- js/jquery-custom.js
+++ js/jquery-custom.js
@@ -1,11 +1,14 @@
-
-    "use strict";
-
-$(function() {
-
-    // INSERT JAVASCRIPT CODE SAMPLE HERE
-    $('p').css('background-color', 'yellow');
-
-
+$(function () {
+    $('li').css('font-size', '20px')
+    const h1 = $('h1').html()
+    $('h1').css('background-color', 'yellow')
+    $('row').css('background-color', 'yellow')
+    $('p').css('background-color', 'yellow')
+    alert(h1)
+    $('h1, p, li').css('background-color', 'yellow')
 
 });
+
+
+/*$('.codeup').css('background-color', '#FF0');
+$('p').css('font-size', '14px');*/
