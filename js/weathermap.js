d03a2bb2fa261fd4fa5ad9f01ced25298e8e7c08
Patrick Mireles <patrickm.mireles@gmail.com>
2023-05-01 14:28:13 -0500

    added img bg

--- js/weathermap.js
+++ js/weathermap.js
@@ -1,27 +1,45 @@
 "use strict";
 
+
+//MAPBOX MAP--
 import { MAPBOX_KEY, OPEN_WEATHER_KEY } from './keys.js';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoicHJlbGkwMyIsImEiOiJjbGd5MXNoMW4wMGphM2dzdTlyb2F5dmhkIn0.ZtkHZdMzPk8vvoJuLvfU5Q';    //starting map marker location
+
+
+mapboxgl.accessToken = MAPBOX_KEY;
+navigator.geolocation.getCurrentPosition(function
+    (position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longtitude;
+
+//starting map marker location
     const map = new mapboxgl.Map({
         container: 'map', // container ID
         style: 'mapbox://styles/mapbox/streets-v12', // style URL
-        center: [-97.7430608,30.267153], // starting position [lng, lat] austin,tx
-        zoom: 12, // starting zoom
+        center: [-97.7430608, 30.267153], // starting position [lng, lat] austin,tx
+        zoom: 12, // starting zoom for pin field selected
+/*
         doubleClickzoom: true,
+
+*/
+    });
     });
 
 
 
 
+//FORECAST API CALL
 $.get(`https://api.openweathermap.org/data/2.5/weather?lat=${30.267153}&lon=${-97.7430608}&appid=${OPEN_WEATHER_KEY}&units=imperial`)
     .done(function(data){
+
         $('#temperature').text('Current Weather in: '+ data.name + ' ' + data.main.temp + '\u00b0 F');
         $('#cloud-condition').text('Cloud Condition: '+ data.weather[0].description);
-        $('#location').text('Current Weather in: '+ data.name + ' ' + data.main.temp + '\u00b0 F');
-        $('#temperature').text('Current Weather in: '+ data.name + ' ' + data.main.temp + '\u00b0 F');
+        $('#feels_like').text('Feels like: '+ data.name + ' ' + data.main.feels_like + '\u00b0 F');
+
+
         console.log(data);
     })
+
     .fail(function (jqXHR,testStatus,errorThrow){
         console.error(errorThrow);
     });
