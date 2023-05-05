eebec211a80f8761ab1176f15552720a59d749e7
Patrick Mireles <patrickm.mireles@gmail.com>
2023-05-05 14:59:54 -0500

    es6 exercises

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,53 @@
+function countWords(sentence) {
+    console.log(sentence)
+    const words = sentence.split(' '); // transform a sentence into an array of words
+    const wordCountObject = words.reduce((wordCounts, word) => {
+        if (typeof wordCounts[word] === 'undefined') {
+            // if the word is not yet present in our object, set it's value to 1
+            wordCounts[word] = 1;
+        } else {
+            // otherwise increment the existing count
+            wordCounts[word] += 1;
+        }
+        return wordCounts;
+    }, {}); // start with an empty object
+    return wordCountObject;
+}
+
+countWords('Mary had a little lamb little lamb little lamb');
+// {Mary: 1, had: 1, a: 1, little: 3, lamb: 3}
+
+
+// next iteration
+const users = [
+    {
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash','python']
+    },
+    {
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript','ruby']
+    },
+    {
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php']
+    },
+    {
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql']
+    },
+    {
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php']
+    }
+];
+// Assume that the users array contains user objects with a `languages` array property
+const usersWithThreeLanguages = users.filter(function(user) {
+    return user.languages.length >= 3;
+    console.log(users)
+});
