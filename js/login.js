94513e1b5f91702a223a3e8c64fcdab0bb9cb4e7
Patrick Mireles <patrickm.mireles@gmail.com>
2023-08-10 13:36:41 -0500

    loginstuff

---
+++ js/login.js
@@ -0,0 +1,23 @@
+"use strict";
+
+document.getElementById("login-form").addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const response = await fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+    });
+
+    if (response.ok) {
+        // Redirect user to authenticated page
+        window.location.href = "/dashboard";
+    } else {
+        alert("Login failed. Please check your credentials.");
+    }
+});
